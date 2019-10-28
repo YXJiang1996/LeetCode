ed6e2b7aa4ee33ce9d19b092b8665dfd6d7d721d
jyx <email@example.com>
2019-10-28 17:28:36 +0800

    Initial commit

---
+++ src/leet_0001_0020/Leet0001_TwoSum.java
@@ -0,0 +1,37 @@
+package leet_0001_0020;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * 题目描述：
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * @author Jiang.YX
+ * @version 1.0
+ * @date 2019/10/28 17:10
+ */
+public class Leet0001_TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return result;
+    }
+}
